9264e0638681d66c45b72e115a6847869b270fa1
Le Xuan Hieu <hieulexuan629@gmail.com>
2022-06-28 00:15:38 +0700

    fix exercise karaoke

--- sybersoft_java18/src/exercise/Karaoke.java
+++ sybersoft_java18/src/exercise/Karaoke.java
@@ -1,47 +1,41 @@
 package exercise;
 
-import java.util.Scanner;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
 
 public class Karaoke {
 
-    static Scanner scanner = new Scanner(System.in);
+    public static float TotalMoney(String int_hour, String out_hour, int water_bottle_num) {
+        float total_money;
+        try {
+            int first_hour_money = 30000;
+            int first_3hour_money = 30000 * 3;
+            float next_first_3hour = (float) (90000 * 30) / 100;
+            int water_bottle_money = water_bottle_num * 10000;
 
-    public static float TotalMoney(int water_bottle) {
-        float total_money = 0.0f;
+            long total_minutes = ChronoUnit.MINUTES.between(LocalTime.parse(int_hour), LocalTime.parse(out_hour));
 
-        float first_hour_money = 30000;
-        float first_3hour_money = 30000 * 3;
-        float next_first_3hour = (float) (90000 * 30) / 100;
-        float water_bottle_money = water_bottle * 10000;
+            if (total_minutes <= 3 * 60) {
+                if (total_minutes % 60 == 0) {
+                    total_money = ((int) (total_minutes / 60) * first_hour_money) + water_bottle_money;
+                } else {
+                    total_money = ((int) ((total_minutes / 60) + 1) * first_hour_money) + water_bottle_money;
+                }
+            } else {
+                long anotherMinutes = total_minutes - 3 * 60;
+                if (anotherMinutes % 60 == 0) {
+                    total_money = (first_3hour_money + (int) (anotherMinutes / 60) * next_first_3hour) + water_bottle_money;
+                } else {
+                    total_money = (first_3hour_money + (int) ((anotherMinutes / 60) + 1) * next_first_3hour) + water_bottle_money;
+                }
+            }
 
-        System.out.println("Enter start hour: ");
-        float in_hour = scanner.nextFloat();
-
-        while (in_hour < 0) {
-            System.out.println("In Hour must to more than 0. Enter against: ");
-            in_hour = scanner.nextFloat();
+            if (LocalTime.parse(int_hour).getHour() >= 9 && LocalTime.parse(out_hour).getHour() <= 17) {
+                total_money = total_money - (total_money * 20) / 100;
+            }
+        } catch (Exception e) {
+            return -1;
         }
-
-        System.out.println("Enter out hour: ");
-        float out_hour = scanner.nextFloat();
-
-        while (out_hour < 0 || out_hour < in_hour) {
-            System.out.println("Out Hour must to more than 0. Enter against: ");
-            out_hour = scanner.nextFloat();
-        }
-
-        float total_hour = out_hour - in_hour;
-
-        if (total_hour <= 3) {
-            total_money = (total_hour * first_hour_money) + water_bottle_money;
-        } else if (total_hour > 3) {
-            total_money = (first_3hour_money + (total_hour - 3) * next_first_3hour) + water_bottle_money;
-        }
-
-        if (in_hour >= 9 && out_hour <= 17) {
-            total_money = (total_money * 20) / 100;
-        }
-
         return total_money;
     }
 }
--- sybersoft_java18/src/test/KaraokeTest.java
+++ sybersoft_java18/src/test/KaraokeTest.java
@@ -2,13 +2,15 @@ package test;
 
 import exercise.Karaoke;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class KaraokeTest {
 
     @Test
     void shouldWorkCorrectly() {
-        assertEquals(50000, Karaoke.TotalMoney(2));
+        assertEquals(-1, Karaoke.TotalMoney("-4:00", "5:00", 2));
+        assertEquals(-1, Karaoke.TotalMoney("4:00", "5:00", 2));
+        assertEquals(50000, Karaoke.TotalMoney("04:00", "05:00", 2));
+        assertEquals(50000, Karaoke.TotalMoney("08:00", "08:59", 2));
     }
 }
